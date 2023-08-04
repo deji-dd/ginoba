279c198fd996dc36553367a73fa118a1ac69184a
deji-dd <dejib50@gmail.com>
2023-08-04 01:19:14 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Do from "./pages/Do";
@@ -13,16 +13,18 @@ import Blog from "./pages/Blog";
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path={"/"} element={<Home />} />
-        <Route path="/about-us" element={<About />} />
-        <Route path="/what-we-do" element={<Do />} />
-        <Route path="/contact-us" element={<Contact />} />
-        <Route path="/get-involved" element={<GetInvolved />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/event" element={<Event />} />
-        <Route path="/blog" element={<Blog />} />
-      </Routes>
+      <BrowserRouter basename="/ginoba">
+        <Routes>
+          <Route path={"/"} element={<Home />} />
+          <Route path="/about-us" element={<About />} />
+          <Route path="/what-we-do" element={<Do />} />
+          <Route path="/contact-us" element={<Contact />} />
+          <Route path="/get-involved" element={<GetInvolved />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/event" element={<Event />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
