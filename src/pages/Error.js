4f86d15606412d43fb63c7208809d86055a661fd
deji-dd <dejib50@gmail.com>
2023-08-04 16:32:19 +0100

    error page fixes

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,60 +1,63 @@
 export default function Error() {
   return (
-    <div
-      style={{
-        width: "90rem",
-        display: "inline-flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        flexDirection: "column",
-        gap: "1rem",
-      }}
-    >
-      <h1
+    <div>
+      <div
         style={{
-          color: "#22495F",
-          fontFamily: "Clash Display",
-          fontSize: "5rem",
-          fontWeight: "600",
-          lineHeight: "4.2rem",
-          letterSpacing: "0.035rem",
+          width: "100vw",
+          display: "inline-flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          flexDirection: "column",
+          gap: "1rem",
         }}
       >
-        404
-      </h1>
-      <p
-        style={{
-          color: "#22495F",
-          fontSize: "1.25rem",
-          fontWeight: "400",
-          lineHeight: "2rem",
-        }}
-      >
-        The page you're looking for doesn't exist.
-      </p>
-      <a href="/" style={{ textDecoration: "none" }}>
-        <button
-          type="button"
+        <h1
           style={{
-            width: "16rem",
-            background: "#096A56",
-            padding: "1rem 4.375rem",
-            color: "white",
-            alignItems: "center",
-            justifyContent: "center",
-            gap: "0.625rem",
-            fontFamily: "Manrope",
-            fontSize: "1.25rem",
+            color: "#22495F",
+            fontFamily: "Clash Display",
+            fontSize: "5rem",
             fontWeight: "600",
-            display: "flex",
-            borderRadius: "0.125rem",
-            border: "1px solid #096A56",
+            lineHeight: "4.2rem",
+            letterSpacing: "0.035rem",
           }}
         >
-          Go Home
-        </button>
-      </a>
+          404
+        </h1>
+        <p
+          style={{
+            color: "#22495F",
+            fontSize: "1.25rem",
+            fontWeight: "400",
+            lineHeight: "2rem",
+            textAlign: "center",
+          }}
+        >
+          The page you're looking for doesn't exist.
+        </p>
+        <a href="/" style={{ textDecoration: "none" }}>
+          <button
+            type="button"
+            style={{
+              width: "16rem",
+              background: "#096A56",
+              padding: "1rem 4.375rem",
+              color: "white",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: "0.625rem",
+              fontFamily: "Manrope",
+              fontSize: "1.25rem",
+              fontWeight: "600",
+              display: "flex",
+              borderRadius: "0.125rem",
+              border: "1px solid #096A56",
+            }}
+          >
+            Go Home
+          </button>
+        </a>
+      </div>
     </div>
   );
 }
